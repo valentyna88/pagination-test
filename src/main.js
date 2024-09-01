c53cd86f9337c30abb14cadee3bfa6a071d3a084
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2024-09-02 00:25:46 +0300

    Update main.js

---
+++ src/main.js
@@ -0,0 +1,6 @@
+// Discovery API (https://developer.ticketmaster.com/products-and-docs/apis/discovery-api/v2/#search-events-v2)
+// API_KEY = 'uHSLi07StIOlriMPxJGxUbSYsHDs6AFx';
+// Потрібно відрендерити колекцію  івентів і реалізувати пагінацію за допомогою бібліотеки
+// tui - pagination(https://www.npmjs.com/package/tui-pagination),
+// Запити робимо використовуючи бібліотеку axios (https://www.npmjs.com/package/axios , https://axios-http.com/uk/docs/intro)
+// і конструкції async\await; try\catch
