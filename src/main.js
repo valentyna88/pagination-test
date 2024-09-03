3c94cc8119b0a22aad171f637314031b0cd238f0
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2024-09-03 22:51:54 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-// Discovery API (https://developer.ticketmaster.com/products-and-docs/apis/discovery-api/v2/#search-events-v2)
+// Discovery API (https://developer.ticketmaster.com/products-and-docs/apis/discovery-api/v2/#search-events-v)2
 // API_KEY = 'uHSLi07StIOlriMPxJGxUbSYsHDs6AFx';
 // Потрібно відрендерити колекцію  івентів і реалізувати пагінацію за допомогою бібліотеки
 // tui - pagination(https://www.npmjs.com/package/tui-pagination),
