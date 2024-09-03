95ce63b9ada35ccc30fbb6c9608e5b81430d56dd
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2024-09-03 22:51:43 +0300

    Update api.js

--- src/js/api.js
+++ src/js/api.js
@@ -3,11 +3,12 @@ import axios from 'axios';
 const API_KEY = 'uHSLi07StIOlriMPxJGxUbSYsHDs6AFx';
 axios.defaults.baseURL = 'https://app.ticketmaster.com/discovery/v2/';
 
-export async function fetchData(page) {
+export async function fetchData(page, value) {
   const asyncParams = {
     params: {
       apikey: API_KEY,
       page,
+      keyword: value,
     },
   };
   const response = await axios.get('events.json', asyncParams);
